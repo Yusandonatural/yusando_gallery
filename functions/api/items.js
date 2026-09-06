54ce6952675171ad41e9237280bafebfbfacbd21
Yusandonatural <isozaki@yusando.com>
2026-09-06 09:33:32 +0900

    new

--- functions/api/items.js
+++
@@ -1,9 +0,0 @@
-import { json, authed, publicItem } from "../_lib.js";
-
-export async function onRequestGet({ request, env }) {
-  const sql = authed(request, env)
-    ? "SELECT * FROM items ORDER BY created_at DESC"
-    : "SELECT * FROM items WHERE status IN ('published','sold') ORDER BY created_at DESC";
-  const { results } = await env.DB.prepare(sql).all();
-  return json(results.map(publicItem));
-}
